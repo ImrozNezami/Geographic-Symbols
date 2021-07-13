ad50da0d19f788ce072b533aef5691510bcbd831
ImrozNezami <62255195+ImrozNezami@users.noreply.github.com>
2021-07-14 03:07:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./styles.css";
+import { useState } from "react";
+var flags = {
+  "🌋": "Volcano",
+  "⛰️": "Mountain",
+  "🏝️": "Desert Island",
+  "🏜️": "Desert",
+  "🏕️": "Camping"
+};
+var flagswehave = Object.keys(flags);
+export default function App() {
+  var [flag, setflag] = useState(" ");
+  function inputtyped(event) {
+    const inputflag = event.target.value;
+    setflag(inputflag);
+    if (flags[inputflag] !== undefined) {
+      setflag(flags[inputflag]);
+    } else setflag("We dont have this symbol");
+  }
+  function emojiclicked(inputflag) {
+    setflag(flags[inputflag]);
+  }
+  return (
+    <div className="App">
+      <h1>Geographic Symbols</h1>
+      <input onChange={inputtyped} />
+      <h2>{flag}</h2>
+      <h3>Symbols will appear here</h3>
+      <hr />
+      <div>
+        {flagswehave.map(function (item) {
+          return <span onClick={() => emojiclicked(item)}>{item}</span>;
+        })}
+      </div>
+    </div>
+  );
+}
